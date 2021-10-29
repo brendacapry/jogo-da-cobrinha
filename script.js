a97b44738fc47a33ea70d175929b452a461abca0
brendacapry <brendacapry@hotmail.com>
2021-10-28 21:03:02 -0400

    definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -21,7 +21,20 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "rigth") direction = "left";
+    if(event.keyCode == 38 && direction != "up") direction = "down";
+    if(event.keyCode == 39 && direction != "left") direction = "rigth";
+    if(event.keyCode == 40 && direction != "down") direction = "up";
+
+}
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "rigth") snake[0].x = 0;
+    if(snake[0].x < 0 && direction =="left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
+    if(snake[0].y < 0 && direction =="down") snake[0].y = 16 * box;
     criarBG();
     criarCobrinha();
 
